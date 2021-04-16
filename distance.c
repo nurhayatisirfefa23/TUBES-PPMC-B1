3dc4d7574ea490f8d183d93e0f8d5add2cccf0d5
Agape D'sky <ikanblackghost@gmail.com>
2021-04-16 22:27:37 +0700

    distance.c initial commit

---
+++ distance.c
@@ -0,0 +1,17 @@
+#include "distance.h"
+
+double distance (double lat1, double lat2, double long1, double long2) {
+    double res;
+
+    //Konversi nilai ke dalam radian
+    double lat1_rad = lat1*M_PI/(float)180; double lat2_rad = lat2*M_PI/(float)180;
+    double long1_rad = long1*M_PI/(float)180; double long2_rad = long2*M_PI/(float)180;
+
+    //Penghitungan
+    res = 2*6371*asin(sqrt(     (sin(0.5*(lat2_rad-lat1_rad)))*(sin(0.5*(lat2_rad-lat1_rad)))
+                                + 
+                                cos(lat1_rad)*cos(lat2_rad)*(sin(0.5*(long2_rad-long1_rad)))*(sin(0.5*(long2_rad-long1_rad)))   ));
+    
+    //Return value
+    return res;
+}
---
+++ distance.h
@@ -0,0 +1,12 @@
+#include "math.h"
+
+/**
+    *Fungsi untuk menghitung jarak berdasarkan input latitude dan longitude
+    *dari 2 buah titik. Mengeluarkan hasil jarak dalam tipe data double
+    
+    *@param lat1 latitude titik 1
+    *@param lat2 latitude titik 2
+    *@param long1 longitude tiitk 1
+    *@param long2 longitude titik 2
+**/
+double distance (double lat1, double lat2, double long1, double long2);
